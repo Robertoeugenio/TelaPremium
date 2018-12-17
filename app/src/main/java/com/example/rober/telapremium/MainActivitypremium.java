f21498111e878386a92ed7c0d52cb58090721e3b
robertoeugenio1978@gmail.com <robertoeugenio>
2018-12-16 22:07:25 -0200

    Initial commit

---
+++ app/src/main/java/com/example/rober/telapremium/MainActivitypremium.java
@@ -0,0 +1,13 @@
+package com.example.rober.telapremium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitypremium extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_premium);
+    }
+}
